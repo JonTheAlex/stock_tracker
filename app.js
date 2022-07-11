7e9c79220a9c4589ce3b481e5f13c82086e20548
JonTheAlex <jon.the.alex@gmail.com>
2022-07-10 20:28:36 -0500

    added views/layouts ejs

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const app = require('express')
+const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const cors = require('cors')
+require('dotenv').config()
 const PORT = 8000
-const dotenv = config()
-
-require('dotenv')
 
 const app = express()
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
-app.use(expressLayouts)
 app.use(express.json())
+app.use(cors())
 
+app.use(expressLayouts)
 app.set('layout', './layouts/main')
 app.set('view engine', 'ejs')
 
 app.get('/', async (request, response) => {
     try {
-        response.render('main.ejs')
+        response.render('index.ejs')
     } catch (error) {
         response.status(500).send({message: error.message})
     }
