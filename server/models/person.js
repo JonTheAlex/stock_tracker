1f27d5b87f6347d00f210ce165785bcb9f6d491a
JonTheAlex <jon.the.alex@gmail.com>
2022-07-24 12:24:03 -0500

    added person schema

---
+++ server/models/person.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'This field is required'
+    },
+    chamber: {
+        type: String,
+        required: 'This field is required'
+    },
+    state: {
+        type: String,
+        required: 'This field is required'
+    },
+    district: {
+        type: String,
+        required: 'This field is required'
+    },
+    party: {
+        type: String,
+        required: 'This field is required'
+    },
+    served: {
+        type: String,
+        required: 'This field is required'
+    }
+    
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema)
