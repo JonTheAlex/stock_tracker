540256c1b57f522ab887af6a63519a7f1ad87619
JonTheAlex <jon.the.alex@gmail.com>
2022-07-24 12:02:48 -0500

    added transaction schema

--- server/models/transaction.js
+++ server/models/transaction.js
@@ -1,12 +1,42 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const transcationSchema = new mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: 'This field is required'
-//     },
-//     transactionDate: {
-//         type: String,
-//         required: String
-//     }
-// })
+const transcationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'This field is required'
+    },
+    status: {
+        type: String,
+        required: 'This field is required'
+    },
+    state_district: {
+        type: String,
+        required: 'This field is required'
+    },
+    owner: {
+        type: String,
+        required: 'This field is required'
+    },
+    asset_name: {
+        type: String,
+        required: 'This field is required'
+    },
+    transaction_type: {
+        type: String,
+        required: 'This field is required'
+    },
+    transaction_date: {
+        type: Date,
+        required: 'This field is required'
+    },
+    notification_date: {
+        type: Date,
+        required: 'This field is required'
+    },
+    amount: {
+        type: String,
+        required: 'This field is required'
+    }
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema)
