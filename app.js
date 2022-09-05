e1cab4156f6a48405a2560caae04ad2214d7f617
JonTheAlex <jon.the.alex@gmail.com>
2022-09-04 21:10:34 -0500

    fixed connect-mongo errors

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(
         secret: 'keyboard cat',
         resave: false,
         saveUninitialized: false,
-        store: new MongoStore({ mongooseConnection: mongoose.connection })
+        store: new MongoStore({ mongooseConnection: mongoose.connection})
     })
 )
 
