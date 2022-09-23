40d68997e19c46a10f6016a3914fb4854d897036
JonTheAlex <jon.the.alex@gmail.com>
2022-09-22 21:37:12 -0500

    made transaction/asset form controller

--- server/controllers/form.js
+++ server/controllers/form.js
@@ -1,7 +1,6 @@
-    /**
-     * GET /
-     * FORM SUBMISSION PAGE
-     */
+const Transaction = require('../models/Transaction')
+const Asset = require('../models/Asset')
+const Person = require('../models/Person')
 
     exports.getForm = (request, response) => {
         try {
@@ -12,5 +11,48 @@
             })
         } catch (error) {
             response.status(500).send({message: error.message})
+        }
+    }
+
+    exports.createTransaction = async (request, response) => {
+        try {
+            const personObjID = await Person.find(req.body.person)
+
+            await Transaction.create({
+                person: personObjID,
+                state: req.body.state,
+                district: req.body.district,
+                asset_name: req.body.asset_name,
+                asset_ticker: req.body.asset_ticker,
+                asset_description: req.body.asset_description,
+                asset_type: req.body.asset_type,
+                transaction_type: req.body.transaction_type,
+                transaction_date: req.body.transaction_date,
+                notification_date: req.body.notification_date,
+                amount: req.body.amount
+            })            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    exports.createAsset = async (request, response) => {
+        try {
+            const personObjID = await Person.find(req.body.person)
+
+            await Asset.create({
+              person: personObjID,
+              asset: req.body.asset,
+              asset_owner: req.body.asset_owner,
+              asset_income_type: req.body.asset_income_type,
+              asset_income: req.body.asset_income,
+              taxable_asset: req.body.taxable_asset,
+              income_source: req.body.income_source,
+              transaction_date: req.body.transaction_date,
+              notification_date: req.body.notification_date,
+              amount: req.body.amount
+            })            
+        } catch (error) {
+            console.log(error)
         }
     }
--- server/routes/form.js
+++ server/routes/form.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const formController = require('../controllers/form')
 
 router.get('/', formController.getForm);
+router.post('/transaction', formController.createTransaction)
+router.post('/asset', formController.createAsset)
 
 module.exports = router
 
