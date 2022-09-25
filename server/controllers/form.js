f6e5496344a17c93ad455b61bb69530f32eef6a4
JonTheAlex <jon.the.alex@gmail.com>
2022-09-25 14:47:09 -0500

    updated form controllers

--- server/controllers/form.js
+++ server/controllers/form.js
@@ -2,245 +2,6 @@ const Transaction = require('../models/Transaction')
 const Asset = require('../models/Asset')
 const Person = require('../models/Person')
 
-    const states = [
-        {
-            "name": "Alabama",
-            "abbreviation": "AL"
-        },
-        {
-            "name": "Alaska",
-            "abbreviation": "AK"
-        },
-        {
-            "name": "American Samoa",
-            "abbreviation": "AS"
-        },
-        {
-            "name": "Arizona",
-            "abbreviation": "AZ"
-        },
-        {
-            "name": "Arkansas",
-            "abbreviation": "AR"
-        },
-        {
-            "name": "California",
-            "abbreviation": "CA"
-        },
-        {
-            "name": "Colorado",
-            "abbreviation": "CO"
-        },
-        {
-            "name": "Connecticut",
-            "abbreviation": "CT"
-        },
-        {
-            "name": "Delaware",
-            "abbreviation": "DE"
-        },
-        {
-            "name": "District Of Columbia",
-            "abbreviation": "DC"
-        },
-        {
-            "name": "Federated States Of Micronesia",
-            "abbreviation": "FM"
-        },
-        {
-            "name": "Florida",
-            "abbreviation": "FL"
-        },
-        {
-            "name": "Georgia",
-            "abbreviation": "GA"
-        },
-        {
-            "name": "Guam",
-            "abbreviation": "GU"
-        },
-        {
-            "name": "Hawaii",
-            "abbreviation": "HI"
-        },
-        {
-            "name": "Idaho",
-            "abbreviation": "ID"
-        },
-        {
-            "name": "Illinois",
-            "abbreviation": "IL"
-        },
-        {
-            "name": "Indiana",
-            "abbreviation": "IN"
-        },
-        {
-            "name": "Iowa",
-            "abbreviation": "IA"
-        },
-        {
-            "name": "Kansas",
-            "abbreviation": "KS"
-        },
-        {
-            "name": "Kentucky",
-            "abbreviation": "KY"
-        },
-        {
-            "name": "Louisiana",
-            "abbreviation": "LA"
-        },
-        {
-            "name": "Maine",
-            "abbreviation": "ME"
-        },
-        {
-            "name": "Marshall Islands",
-            "abbreviation": "MH"
-        },
-        {
-            "name": "Maryland",
-            "abbreviation": "MD"
-        },
-        {
-            "name": "Massachusetts",
-            "abbreviation": "MA"
-        },
-        {
-            "name": "Michigan",
-            "abbreviation": "MI"
-        },
-        {
-            "name": "Minnesota",
-            "abbreviation": "MN"
-        },
-        {
-            "name": "Mississippi",
-            "abbreviation": "MS"
-        },
-        {
-            "name": "Missouri",
-            "abbreviation": "MO"
-        },
-        {
-            "name": "Montana",
-            "abbreviation": "MT"
-        },
-        {
-            "name": "Nebraska",
-            "abbreviation": "NE"
-        },
-        {
-            "name": "Nevada",
-            "abbreviation": "NV"
-        },
-        {
-            "name": "New Hampshire",
-            "abbreviation": "NH"
-        },
-        {
-            "name": "New Jersey",
-            "abbreviation": "NJ"
-        },
-        {
-            "name": "New Mexico",
-            "abbreviation": "NM"
-        },
-        {
-            "name": "New York",
-            "abbreviation": "NY"
-        },
-        {
-            "name": "North Carolina",
-            "abbreviation": "NC"
-        },
-        {
-            "name": "North Dakota",
-            "abbreviation": "ND"
-        },
-        {
-            "name": "Northern Mariana Islands",
-            "abbreviation": "MP"
-        },
-        {
-            "name": "Ohio",
-            "abbreviation": "OH"
-        },
-        {
-            "name": "Oklahoma",
-            "abbreviation": "OK"
-        },
-        {
-            "name": "Oregon",
-            "abbreviation": "OR"
-        },
-        {
-            "name": "Palau",
-            "abbreviation": "PW"
-        },
-        {
-            "name": "Pennsylvania",
-            "abbreviation": "PA"
-        },
-        {
-            "name": "Puerto Rico",
-            "abbreviation": "PR"
-        },
-        {
-            "name": "Rhode Island",
-            "abbreviation": "RI"
-        },
-        {
-            "name": "South Carolina",
-            "abbreviation": "SC"
-        },
-        {
-            "name": "South Dakota",
-            "abbreviation": "SD"
-        },
-        {
-            "name": "Tennessee",
-            "abbreviation": "TN"
-        },
-        {
-            "name": "Texas",
-            "abbreviation": "TX"
-        },
-        {
-            "name": "Utah",
-            "abbreviation": "UT"
-        },
-        {
-            "name": "Vermont",
-            "abbreviation": "VT"
-        },
-        {
-            "name": "Virgin Islands",
-            "abbreviation": "VI"
-        },
-        {
-            "name": "Virginia",
-            "abbreviation": "VA"
-        },
-        {
-            "name": "Washington",
-            "abbreviation": "WA"
-        },
-        {
-            "name": "West Virginia",
-            "abbreviation": "WV"
-        },
-        {
-            "name": "Wisconsin",
-            "abbreviation": "WI"
-        },
-        {
-            "name": "Wyoming",
-            "abbreviation": "WY"
-        }
-    ]
-
     exports.getForm = (request, response) => {
         try {            
             response.render('form', {
@@ -255,44 +16,46 @@ const Person = require('../models/Person')
 
     exports.createTransaction = async (request, response) => {
         try {
-            const personObjID = await Person.find(req.body.person)
-
-            await Transaction.create({
-                person: personObjID,
-                state: req.body.state,
-                district: req.body.district,
-                asset_name: req.body.asset_name,
-                asset_ticker: req.body.asset_ticker,
-                asset_description: req.body.asset_description,
-                asset_type: req.body.asset_type,
-                transaction_type: req.body.transaction_type,
-                transaction_date: req.body.transaction_date,
-                notification_date: req.body.notification_date,
-                amount: req.body.amount
-            })            
+            data = {
+                person: request.body.personTransaction,
+                asset_owner: request.body.asset_owner,
+                asset_name: request.body.asset_name,
+                asset_ticker: request.body.asset_ticker,
+                asset_description: request.body.asset_description,
+                asset_type: request.body.asset_type,
+                transaction_type: request.body.transaction_type,
+                transaction_date: request.body.transaction_date,
+                notification_date: request.body.notification_transaction_date,
+                amount: request.body.amountTransaction
+            }
+            console.log(data)
+            console.log('Transaction Created')
         } catch (error) {
             console.log(error)
+            console.log('Transaction Failed')
         }
     }
 
     exports.createAsset = async (request, response) => {
         try {
-            const personObjID = await Person.find(req.body.person)
-
-            await Asset.create({
-              person: personObjID,
-              asset: req.body.asset,
-              asset_owner: req.body.asset_owner,
-              asset_income_type: req.body.asset_income_type,
-              asset_income: req.body.asset_income,
-              taxable_asset: req.body.taxable_asset,
-              income_source: req.body.income_source,
-              transaction_date: req.body.transaction_date,
-              notification_date: req.body.notification_date,
-              amount: req.body.amount
-            })            
+            data = {
+              person: request.body.personAsset,
+              asset_name: request.body.asset_name,
+              asset_description: request.body.asset_description,
+              asset_owner: request.body.asset_owner,
+              asset_value: request.body.asset_value,
+              asset_income_type: request.body.asset_income_type,
+              asset_income_amount: request.body.asset_income_amount,
+              asset_transaction_type: request.body.asset_transaction_type,
+              transaction_date: request.body.transaction_asset_date,
+              notification_date: request.body.notification_asset_date,
+              amount: request.body.amountAsset
+            }
+            console.log(data)
+            console.log('Asset Created')  
         } catch (error) {
             console.log(error)
+            console.log('Asset Failed')
         }
     }
 
@@ -300,7 +63,6 @@ const Person = require('../models/Person')
         try {
             const personData = await Person.find({}, 'name')
             let dropDown = {
-                'states': states,
                 'persons': personData,
                 'updated': Date.now()
             }
