2abcf17789ca58043e26734b82ab0cdd9065934e
JonTheAlex <jon.the.alex@gmail.com>
2022-12-05 12:37:42 -0600

    changed port

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use('/', mainRoutes)
 app.use('/blog', postsRoutes)
 app.use('/form', formRoutes)
 
-//PORT = 8000
+//PORT = 8080
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
 })
